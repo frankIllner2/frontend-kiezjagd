3c6e5c120fd2ce49b2e89a20f663fe96bfa2390c
FrankIllner <frankillner@web.de>
2025-01-09 16:24:48 +0100

    manifest korrektur über vue.config

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,22 @@ module.exports = {
     name: 'Kiezjagd',
     themeColor: '#4DBA87',
     manifestOptions: {
-      short_name: 'Spiel',
+      short_name: 'Kiez-Spiel',
       start_url: '/',
       display: 'standalone',
       background_color: '#ffffff',
+      icons: [
+        {
+          src:  '/img/icons/web-app-manifest-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/img/icons/web-app-manifest-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ]
     },
     workboxOptions: {
       runtimeCaching: [
