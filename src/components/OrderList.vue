0ba076f43eddf8b80e3f1d0c8e6783cc928607c2
FrankIllner <frankillner@web.de>
2025-05-29 17:13:41 +0200

    admin Bestllungen

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="orders-list">
-    <h2>Bestellungen</h2>
     <div v-if="orders.length === 0" class="no-orders">
       <p>Keine Bestellungen gefunden.</p>
     </div>
     <div v-for="order in orders" :key="order._id" class="order-card">
-      <h3>{{ order.gameName }}</h3>
-      <h4>{{ order.invoiceNumber }}</h4>
+      <b>{{ order.gameName }}</b>
+      <p><strong v-if="order.invoiceNumber">Rechnungsnr.:</strong>{{ order.invoiceNumber }}</p>
       <p><strong>Kunde:</strong> {{ order.email }}</p>
       <p><strong>Gekauft am:</strong> {{ formatEndTime(order.createdAt) }}</p>
       <p><strong>Spiel-ID:</strong> {{ order.gameId }}</p>
